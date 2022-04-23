db9ce06bcb8c32dad40c6275911309ae8e72d7c7
Armin1-lab <silas.oliveira8@gmail.com>
2022-04-22 22:06:59 -0300

    more tests

--- src/tests/unit/controllers/controller.test.ts
+++ src/tests/unit/controllers/controller.test.ts
@@ -9,22 +9,50 @@ import { Response } from "express";
 import CarController from "../../../controllers/Car.controller";
 
 
-describe('Test the "Controller" layer', () => {
+describe.only('Test the "Controller" layer', () => {
   const carController = new CarController();
-  beforeEach(sinon.restore);
+
+
   describe('test the ("create") method', () => {
     const req = { body: carMockParams } as RequestWithBody<Car>;
-    const res = {} as Response;
+    const res = { } as Response;
 
     before(() => {
       res.status = sinon.stub().returns(res);
-      res.json = sinon.stub();
+      res.json = sinon.stub().returns(carMock);
       sinon.stub(carController.service, "create").resolves(carMock);
     });
+
+    after(() => {
+      sinon.restore();
+    })
 
     it("test when the function created worked", async () => {
       await carController.create(req, res);
       expect((res.status as sinon.SinonStub).calledWith(201));
+      expect((res.json as sinon.SinonStub).calledWith(carMock));
+    });
+  });
+
+
+  describe('test the ("create") method with empty body', () => {
+    const req = { body: {} } as RequestWithBody<Car>;
+    const res = { } as Response;
+
+    before(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns({ });
+      sinon.stub(carController.service, "create").resolves({ } as any);
+    });
+
+    after(() => {
+      sinon.restore();
+    })
+
+    it("test when the function created worked", async () => {
+      await carController.create(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(500));
+      expect(res.json({ }));
     });
   });
 });
